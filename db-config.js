3dca10ebe88075d68c61102ffae31a79d17c6886
HannoSpreeuw <h.spreeuw@esciencecenter.nl>
2019-02-05 18:50:50 +0100

    Users should get their own credentials for LOFAR by visiting https://lofar.astron.nl/useradministration/public/setUpUserAccount.do

--- db-config.js
+++ db-config.js
@@ -2,8 +2,8 @@ module.exports = {
  databaseURL: process.env.DATABASE_URL || "postgres://postgres@localhost/frbcat",
  csvURL: process.env.CSV_URL || "http://www.frbcat.org/frbcat.csv",
   hrPool: {
-    user: process.env.HR_USER || 'hspreeuw',
-    password: process.env.HR_PASSWORD || 'gxI4h2h09B6E22dNYpf7',
+    user: process.env.HR_USER || 'your username from https://lofar.astron.nl/useradministration/public/setUpUserAccount.do',
+    password: process.env.HR_PASSWORD || 'your password from https://lofar.astron.nl/useradministration/public/setUpUserAccount.do',
     connectString: process.env.HR_CONNECTIONSTRING || 'db.lofar.target.rug.nl:1521/db.lofar.target.rug.nl',
     poolMin: 10,
     poolMax: 10,
